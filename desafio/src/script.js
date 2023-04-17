c774e25f5216a8ec4a5886c20cc76ab01329ceac
Cristhian Tapiero <cristapi56@gmail.com>
2023-04-16 20:41:50 -0500

    Agregando funcionalidades a los botones

--- desafio/src/script.js
+++ desafio/src/script.js
@@ -1,21 +1,32 @@
+const textArea = document.getElementById('text-area')
+const encryptButton = document.getElementById('encriptar')
+const desencryptButton = document.getElementById('desencriptar')
+const copyButton = document.getElementById('copy')
+const noResult = document.getElementById('no-result-section')
+const resultSection = document.getElementById('result-section')
+const resultText = document.getElementById('result-text')
+
+
 const encriptar = (texto) =>{
     let resultado = texto
-    resultado = resultado.replace("e", "enter")
-    resultado = resultado.replace("i", "imes")
-    resultado = resultado.replace("a", "ai")
-    resultado = resultado.replace("o", "ober")
-    resultado = resultado.replace("u","ufat")
+    resultado = resultado.replace(/e/img, "enter")
+    resultado = resultado.replace(/i/img, "imes")
+    resultado = resultado.replace(/a/img, "ai")
+    resultado = resultado.replace(/o/img, "ober")
+    resultado = resultado.replace(/u/img,"ufat")
     return resultado
 }
+
 const desencriptar = (texto) =>{
     let resultado = texto
-    resultado = resultado.replace("enter", "e")
-    resultado = resultado.replace("imes", "i")
-    resultado = resultado.replace("ai", "a")
-    resultado = resultado.replace("ober", "o")
-    resultado = resultado.replace("ufat", "u")
+    resultado = resultado.replace(/enter/img, "e")
+    resultado = resultado.replace(/imes/img, "i")
+    resultado = resultado.replace(/ai/img, "a")
+    resultado = resultado.replace(/ober/img, "o")
+    resultado = resultado.replace(/ufat/img, "u")
     return resultado
 }
+
 const copiar = (texto) =>{
     const range = document.createRange()
     range.selectNode(texto)
@@ -25,3 +36,34 @@ const copiar = (texto) =>{
     window.getSelection().removeAllRanges()
 
 }
+
+encryptButton.addEventListener('click', (e)=>{
+    e.preventDefault();
+    if(textArea.value != ''){
+        noResult.classList.add('invisible')
+        resultSection.classList.remove('invisible')
+        resultText.innerHTML = `${encriptar(textArea.value)}`
+    }else{
+        noResult.classList.remove('invisible')
+        resultSection.classList.add('invisible')
+    }
+})
+
+desencryptButton.addEventListener('click', (e)=>{
+    e.preventDefault();
+    if(textArea.value != ''){
+        noResult.classList.add("invisible")
+        resultSection.classList.remove("invisible")
+        resultText.innerHTML = desencriptar(textArea.value)
+    }else{
+        noResult.classList.remove('invisible')
+        resultSection.classList.add('invisible')
+    }
+})
+
+copyButton.addEventListener('click', (e)=>{
+    e.preventDefault();
+    copiar(resultText)
+})
+
+console.log(encriptar('Cristhian'));
